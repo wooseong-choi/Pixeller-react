af95232abe1d8bef25cb9465a54c81c4d23a4592
Digi24-dev <digi7442@gmail.com>
2024-07-08 14:38:46 +0900

    REFACTOR: 로그인 부분 모듈화

--- src/components/ModalLogin.jsx
+++ src/components/ModalLogin.jsx
@@ -1,12 +1,9 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
 import GLogin from "./GLogin";
 import "../static/css/ModalLogin.css";
-import { setCookie } from "./Cookies.ts";
-import { jwtDecode } from "jwt-decode";
 import { UserDTO } from "../api/dto/user.js";
-import { login } from "../api/login.jsx";
+import { loginS } from "../api/login.jsx";
 
 const ModalLogin = ({ isOpen, onClose }) => {
   const [username, setUsername] = useState("");
@@ -24,31 +21,9 @@ const ModalLogin = ({ isOpen, onClose }) => {
   const handleLogin = async () => {
     const user = new UserDTO(username, password);
 
-    try {
-      const response = await login(user);
-      if (response === null || response === "")
-        return alert("로그인이 실패하였습니다.");
-      if (response.msg === "Ok") {
-        if (response.jwt) {
-          const option = {
-            Path: "/",
-            HttpOnly: true,
-            SameSite: "None",
-            Secure: true,
-            expires: new Date(new Date().getTime() + 60 * 60 * 1000 * 24 * 14),
-          };
-          setCookie("refresh_token", response.jwt, option);
-        }
-        
-        sessionStorage.setItem("user", response.jwt);
-        sessionStorage.setItem("username", username);
-        navigate("/main");
-      } else {
-        alert(response.msg);
-      }
-    } catch (error) {
-      console.log(error);
-      alert("에러가 발생했습니다.");
+    const res = await loginS(user);
+    if (res === "success") {
+      navigate("/main");
     }
   };
 
