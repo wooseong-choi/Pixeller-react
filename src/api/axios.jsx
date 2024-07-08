5a3f9736db3dace4a01db8a87dbbecf61ddf5ad8
류강현 <rkdgus9394@gmail.com>
2024-07-09 01:26:36 +0900

    test

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 // axios.defaults.withCredentials = true;
 
 const axiosInstance = axios.create({
-  baseURL: "http://43.202.105.144:3333", // Change this to Backend API URL
+  baseURL: "https://pixeller.net:3333", // Change this to Backend API URL
   timeout: 1000,
   Headers: {
     "Content-Type": "application/json",
