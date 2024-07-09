9b55229d180e58bac366447c204e5a625a31bf65
류강현 <rkdgus9394@gmail.com>
2024-07-10 00:51:58 +0900

    strictmode 주석처리

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import {CookiesProvider} from 'react-cookie';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <CookiesProvider>
       <App />
     </CookiesProvider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
