19acb9a4ec5e33375c7be0dd8cc26426320154b6
Digi24-dev <digi7442@gmail.com>
2024-07-10 16:37:38 +0900

    FEAT: Auction 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Main from "./pages/Main.jsx";
 import ProductCreate from "./components/Boards/ProductCreate.jsx";
 import ProductDetail from "./components/Boards/ProductCreate.jsx";
 import ProductList from "./components/Boards/ProductList.jsx";
+import Auction from "./components/Auction/Auction.jsx";
 import "./static/css/App.css";
 
 function App() {
@@ -27,6 +28,7 @@ function App() {
         <Route path="/PC" element={<ProductCreate />} />
         <Route path="/PD" element={<ProductDetail />} />
         <Route path="/PL" element={<ProductList />} />
+        <Route path="/AC" element={<Auction />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
