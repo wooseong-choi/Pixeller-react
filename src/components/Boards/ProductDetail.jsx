01670a4f281d70ac5c3b0fd8659620037a862040
Digi24-dev <digi7442@gmail.com>
2024-07-11 22:08:43 +0900

    chore: 상품 관련 프론트 코드 작성중

--- src/components/Boards/ProductDetail.jsx
+++ src/components/Boards/ProductDetail.jsx
@@ -1,21 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { getProductById } from "../../api/products";
 import "./PD.css";
 import UserInfo from "../UI/UserInfo";
 
 const ProductDetail = ({ productId, handleClose, setAuctionProduct }) => {
-  console.log("in PD: ", productId);
+  const [product, setProduct] = useState({
+    productId: "",
+    name: "",
+    price: "",
+    memberId: "",
+  });
+
   useEffect(() => {
-    // getProductById(productId).then((res) => {
-    //   console.log("Product Detail: ", res);
-    // });
+    getProductById(productId.id).then((res) => {
+      console.log(res);
+      setProduct(res);
+    });
   }, []);
-  
+
   const handleSetAuctionProduct = (productId) => {
     setAuctionProduct(productId);
   };
+
   const user = sessionStorage.getItem("user");
-  return ( 
+
+  return (
     <div className="board-wrapper">
       <div className="board-container">
         <button className="close-button" onClick={handleClose}>
@@ -27,29 +36,30 @@ const ProductDetail = ({ productId, handleClose, setAuctionProduct }) => {
         <div className="product-detail">
           <div className="product-seller">
             <span>판매자:</span>
-            <UserInfo user={user}/>
+            <UserInfo user={user} />
           </div>
           <div className="product-container">
             <div className="product-info">
-              <div className="product-title">상품 제목</div>
-              <div className="product-price">상품 가격</div>
+              <div className="product-title">상품: {product.name}</div>
+              <div className="product-price">￦ {product.price}</div>
               <div className="product-content">
-                상품 설명 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Etiam auctor eros ac dapibus consectetur.
+                상품 설명 : {product.description}
               </div>
             </div>
             <div className="product-wantBuyer">
               <p>구매 요청자 목록</p>
               <div className="buyer-list">
-                
-                <UserInfo user={user}/>
-                <UserInfo user={user}/>
+                <UserInfo user={user} />
+                <UserInfo user={user} />
               </div>
             </div>
             <div className="product-button">
-              <button className="join-auction" onClick={() => {
+              <button
+                className="join-auction"
+                onClick={() => {
                   handleSetAuctionProduct(productId);
-              } }>
+                }}
+              >
                 경매 참여하기
               </button>
               <button className="product-request">구매 요청하기</button>
