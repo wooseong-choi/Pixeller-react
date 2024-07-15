ad11c11b3dbf49fa2845efd06f5ce86923ff97f3
Digi24-dev <digi7442@gmail.com>
2024-07-15 19:25:23 +0900

    feat: 상단 경매 최대 금액 표시

---
+++ src/components/Auction/Auction_max_bid.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import { useSpring, animated } from "@react-spring/web";
+import {} from "../../api/bidding";
+import { _config } from "gsap/gsap-core";
+
+const Auction_max_bid = ({ price }) => {
+  const [prevPrice, setPrevPrice] = React.useState(0);
+
+  const [styles, api] = useSpring(() => ({
+    from: { backgroundColor: "white" },
+  }));
+
+  useEffect(() => {
+    if (price !== prevPrice) {
+      api.start({
+        to: [{ backgroundColor: "green" }, { backgroundColor: "white" }],
+        _config: { duration: 300 },
+      });
+      setPrevPrice(price);
+    }
+  });
+
+  return (
+    <>
+      <div className="max-bid-wrapper">
+        <div className="max-bid-container">
+          <div className="max-bid-data">
+            <animated.div style={styles} className="max-bid-price">
+              {price.toLocaleString()} 원
+            </animated.div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Auction_max_bid;
