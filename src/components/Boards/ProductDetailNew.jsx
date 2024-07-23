119c15e0ff8eaad0c42aa59d4503500c21a11612
최우성 <ghkdgo8686@gmail.com>
2024-07-24 01:23:32 +0900

    클릭 to 슬라이드

--- src/components/Boards/ProductDetailNew.jsx
+++ src/components/Boards/ProductDetailNew.jsx
@@ -9,27 +9,7 @@ import Swiper from "swiper/bundle";
 import "../../static/css/swiper-bundle.min.css";
 
 const ProductDetailNew = ({ productData }) => {
-  // const [product, setProduct] = useState({
-  //   name: "",
-  //   price: "",
-  //   description: "",
-  //   fileImage: [],
-  // });
-
-  // useEffect(() => {
-  //   getProductById(productId).then((res) => {
-  //     console.log("DEBUG: Auction 상품 정보", res);
-  //     setProduct({
-  //       name: res.name,
-  //       price: res.price,
-  //       description: res.description,
-  //       fileImage: res.imageFileUrls,
-  //     });
-  //   });
-  // }, [productId]);
-
-  // productData.fileImage = ["모짜르트.jpg", "누군지모름.jpg", "베토벤.jpg"];
-
+  
   // 이미지 swiper
   useEffect(() => {
     if (productData.fileImage.length > 0) {
@@ -44,7 +24,6 @@ const ProductDetailNew = ({ productData }) => {
 
   return (
     <div className="product-detail-new">
-      {/* <h2>트랙패드 by Ryuu</h2> */}
       <h2>{productData.name}</h2>
       <div className="product-detail-div">
         <div className="swiper-container swiper">
@@ -65,21 +44,9 @@ const ProductDetailNew = ({ productData }) => {
       {productData.description.split("\n").map((line, index) => (
         <p key={index}>{line}</p>
       ))}
-      {/* <p>맥북 사용하시는 분! 아니면 윈도우 사용하시는 분이더라도</p>
-      <p>트랙패드는 꼭꼭꼭! 필요할거에요!!!</p>
-      <br />
-      <p>
-        진짜 트랙패드 한 번 사용해보시면 마우스 절대로 사용하지 못 하실거에요
-      </p>
-      <br />
-      <p>저는 이거 전남자친구한테 선물 받았는데요</p>
-      <p>헤어져서 팔게요....</p>
-      <br />
-      <p>다른 분들이 잘 사용해주세요.,. ㅜㅜ</p> */}
       <div className="starting-price">
         <span>Starting price</span>
-        {/* <span>$5,000 {product.price}</span> */}
-        <span>₩ {productData.price}</span>f
+        <span>₩ {productData.price}</span>
       </div>
     </div>
   );
--- src/components/UI/bottommenu/bgm.jsx
+++ src/components/UI/bottommenu/bgm.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Bgm = ({clickPauseHandler, bgmRunning}) => {
   
+  useEffect(() => {
+    const audioElement = document.getElementById('audio');
+    if (audioElement) {
+      audioElement.volume = 0.4; // 볼륨을 절반으로 설정
+    }
+  }, [bgmRunning]); // bgmRunning이 변경될 때마다 실행
+
   return (
     <div className="bottom_menu_item pause" onClick={clickPauseHandler}>
         {bgmRunning ? <audio id="audio" src="bgm/MapleStory_-_Raindrop_Flower.mp3" autoPlay loop ></audio> : null}
